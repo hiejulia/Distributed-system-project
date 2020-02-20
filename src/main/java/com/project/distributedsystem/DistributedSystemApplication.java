3495d59504a8d8ae98b7cb473652dca1bea17416
hien.nguyen <hien.nguyen@elisa.fi>
2020-02-20 17:33:27 +0200

    init project

---
+++ src/main/java/com/project/distributedsystem/DistributedSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.distributedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedSystemApplication.class, args);
+    }
+
+}
